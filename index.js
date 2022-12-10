a751b7d5dd962fbfc4f395a64249459685217bf4
Sisilisko <beloborodova.viktoriia@gmail.com>
2022-12-10 13:03:37 +0200

    feat: add code section

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ for (let elm of elements) {
 }
 //END animation of logo
 
+//START code example
+function getRandomColor()
+{
+  var randomColor = "#";
+  var hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A","B","C","D","E","F"];
+  for(var i = 0; i < 6; i++)
+  {
+    randomColor += hexNumbers[Math.floor(Math.random()*16)];
+  }
+  document.getElementById("code").style.backgroundColor = randomColor;
+  document.getElementById("color-code").innerHTML = randomColor;
+}
+//END code example
