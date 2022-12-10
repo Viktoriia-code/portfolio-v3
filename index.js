dad0100a4f1d4b384aad9c71e436763f1e086bff
Sisilisko <beloborodova.viktoriia@gmail.com>
2022-12-10 20:51:43 +0200

    feat: add btn scroll to top

--- index.js
+++ index.js
@@ -30,3 +30,16 @@ function getRandomColor()
   document.getElementById("color-code").innerHTML = randomColor;
 }
 //END code example
+
+//button to scroll back to top
+const btnScrollToTop = document.getElementById("btnToTop");
+btnScrollToTop.addEventListener("click", function() { 
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" }); 
+}); 
+window.addEventListener("scroll", () => { 
+  if (window.pageYOffset > 100) { 
+    btnScrollToTop.classList.add("active"); 
+  } else { 
+    btnScrollToTop.classList.remove("active"); 
+  } 
+});
